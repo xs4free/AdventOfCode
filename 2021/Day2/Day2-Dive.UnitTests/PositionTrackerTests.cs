83bfc1fa5cca1ba2a70c89f3170430becb3f048d
Rogier Reedijk <github@progz.nl>
2021-12-19 22:09:15 +0100

    Add skeleton projects day 2

---
+++ 2021/Day2/Day2-Dive.UnitTests/PositionTrackerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Day2_Dive.UnitTests
+{
+    public class PositionTrackerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ 2021/Day2/Day2-Dive/PositionTracker.cs
@@ -0,0 +1,20 @@
+﻿namespace Day2_Dive
+{
+    public class PositionTracker
+    {
+        int horizontalPosition;
+        int depth;
+
+        public void Forward(int steps)
+        {
+        }
+
+        public void Down(int steps)
+        {
+        }
+
+        public void Up(int steps)
+        { 
+        }
+    }
+}
