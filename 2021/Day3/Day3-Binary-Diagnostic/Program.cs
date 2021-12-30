dabf4a0412b07dcd61e051f996a34b9d4663141d
Rogier Reedijk <github@progz.nl>
2021-12-30 23:15:25 +0100

    Parse input and move input to correct subfolder

--- 2021/Day3/Day3-Binary-Diagnostic/Program.cs
+++ 2021/Day3/Day3-Binary-Diagnostic/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day3_Binary_Diagnostic;
+
+string[] report = await File.ReadAllLinesAsync(@"..\..\..\..\input.txt", System.Text.Encoding.UTF8);
+Console.WriteLine($"Report lines: {report.Length}");
+
+DiagnosticReportAnalyzer analyzer = new();
+var result = analyzer.Analyze(report);
+
+Console.WriteLine($"Gamma rate: {result.GammaRate}");
+Console.WriteLine($"Epsilon rate: {result.EpsilonRate}");
+Console.WriteLine($"Power consumption: {result.PowerConsumption}");
