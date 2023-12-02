65408b07f7602756c997966afd7164ee535ec335
Rogier Reedijk <github@progz.nl>
2023-12-03 00:02:21 +0100

    Add Day 2 - part 1

---
+++ 2023/Day2/CubeConundrum/CubeConundrumSolver.cs
@@ -0,0 +1,29 @@
+﻿namespace CubeConundrum;
+
+public static class CubeConundrumSolver
+{
+    public static int GetSumOfPossibleGameIds(string bagContents, string[] games)
+    {
+        var bag = GameParser.ParseBagContents(bagContents);
+        var parsedGames = GameParser.Parse(games);
+        
+        var possibleGames = GetPossibleGames(bag, parsedGames);
+        
+        return possibleGames.Select(game => game.Id).Sum();
+    }
+    
+    private static IEnumerable<Game> GetPossibleGames(BagContents bagContents, IEnumerable<Game> games)
+    {
+        return games.Where(game => IsGamePossibleWithBag(bagContents, game)).ToList();
+    }
+
+    private static bool IsGamePossibleWithBag(BagContents bag, Game game)
+    {
+        return game.Draws.All(draw => IsDrawPossibleWithBag(bag, draw));
+    }
+
+    private static bool IsDrawPossibleWithBag(BagContents bag, CubesDraw draw)
+    {
+        return draw.Red <= bag.Red && draw.Green <= bag.Green && draw.Blue <= bag.Blue;
+    }
+}
---
+++ 2023/Day2/CubeConundrum/Entities.cs
@@ -0,0 +1,7 @@
+﻿namespace CubeConundrum;
+
+internal record Game(int Id, IEnumerable<CubesDraw> Draws);
+internal record CubesDraw(int Blue = 0, int Red = 0, int Green = 0);
+internal record BagContents(int Blue = 0, int Red = 0, int Green = 0);
+
+internal enum CubeColors { Blue, Red, Green };
